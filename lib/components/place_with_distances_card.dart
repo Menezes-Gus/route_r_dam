6c53356efa8111fddb82decf2bd3d566cccb2c86
Bhscjhdvds <92058672+Menezes-Gus@users.noreply.github.com>
2022-02-12 15:27:11 -0300

    main

---
+++ lib/components/place_with_distances_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaceWithDistancesCard extends StatelessWidget {
+  const PlaceWithDistancesCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
